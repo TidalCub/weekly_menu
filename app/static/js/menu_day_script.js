4093c729c85d6459e685d89fa04a78566ddcf159
TidalCub <Leon.skinner@hotmail.co.uk>
2022-11-26 19:36:52 +0000

    add diffrent day selected

--- app/static/js/menu_day_script.js
+++ app/static/js/menu_day_script.js
@@ -3,4 +3,24 @@ function current_day(){
     return date.toLocaleDateString("en", { weekday: 'long' });  
 }
 
-console.log(current_day())
+function expand_current_day(){
+    var day = current_day()
+    var body =document.getElementById(day) 
+    body.classList.add("active-day");
+    
+}
+
+function clicked_day_check(day){
+    remove_active_day()
+    var day_box = document.getElementById(day) 
+    day_box.classList.add("active-day");
+}
+
+function remove_active_day(){
+    var day_arr = document.getElementsByClassName("active-day")
+    Array.from(day_arr).forEach((day_arr) => {
+        day_arr.classList.remove("active-day")
+    })
+}
+
+expand_current_day()
